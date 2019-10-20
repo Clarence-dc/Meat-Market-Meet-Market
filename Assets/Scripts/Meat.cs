9a258825d959e30112fcb76eca0174d8abc4370c
Hannah <theintmyat_wai@mymail.sutd.edu.sg>
2019-10-20 23:25:26 +0800

    pick up, drop, and throw meat scripts, and the meat object and material

---
+++ Assets/Scripts/Meat.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Meat : MonoBehaviour
+{
+    [SerializeField]
+    private Text pickUpText;
+
+    private bool pickUpAllowed;
+
+    private bool canThrow;
+    private bool moving;
+
+
+    // Use this for initialization
+    private void Start()
+    {
+        pickUpText.gameObject.SetActive(false);   
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (pickUpAllowed && Input.GetKeyDown(KeyCode.Space))
+            pickUp();
+        if (canThrow && Input.GetKeyDown(KeyCode.T))
+            throwDrop();
+        if (moving)
+            transform.Translate(transform.forward * Time.deltaTime);   
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            pickUpText.gameObject.SetActive(true);
+            pickUpAllowed = true;
+
+        }
+
+    }
+
+  
+
+    private void pickUp()
+    {
+        Debug.Log("I'm being picked up");
+        GameObject PlayerObject = GameObject.Find("Player");
+        transform.SetParent(PlayerObject.transform);
+        pickUpAllowed = false;
+        pickUpText.gameObject.SetActive(false);
+        canThrow = true;
+    }
+
+    private void throwDrop()
+    {
+        transform.SetParent(null);
+        moving = true;
+        
+    }
+
+}
