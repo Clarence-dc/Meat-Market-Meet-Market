748a111988f37091edf593417541fc865efcd2f8
cirticus <cirticus@gmail.com>
2019-11-11 00:09:59 +0800

    Added bell sound at start

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioSource Bang;
+    public AudioSource Bell;
+    public AudioSource GetOut;
+    public AudioSource Defeat;
+
+    public void PlayBang()
+    {
+        Bang.Play();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Bell.Play();
+    }
+}
