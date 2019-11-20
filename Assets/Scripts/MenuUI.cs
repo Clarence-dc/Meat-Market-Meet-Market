0a08be714ff91a6775d1146d0b0897224215bb10
janjellies <56679981+janjellies@users.noreply.github.com>
2019-11-20 23:02:07 +0800

    menu start

---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("Layout");
+    }
+    public void BackToMenu()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("menu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
